4c41f0afab0cc24be7eb9abe9e8f1394317827ba
Mwni <manuel@tigmar.de>
2023-09-25 20:09:18 +0200

    add settings tab with import/export feature

--- sch0ngesehen.js
+++ sch0ngesehen.js
@@ -25,6 +25,19 @@ class App{
 		this.checkState()
 	}
 
+	static checkSettings(){
+		if(!this.isOnSettingsPage())
+			return
+
+		if(this.isLoading()){
+			log('[App] settings page is loading. polling...')
+			setTimeout(() => this.checkSettings(), 100)
+			return
+		}
+
+		Settings.init()
+	}
+
 	static scheduleStateCheck(){
 		if(this.checkTimeout){
 			clearTimeout(this.checkTimeout)
@@ -34,11 +47,10 @@ class App{
 	}
 
 	static checkState(){
-		if(!this.isOnSubjectPage())
-			return
-
 		if(window.location.href === this.url)
 			return
+
+		log('[App] check state')
 
 		if(this.isLoading()){
 			log('[App] content is loading. polling...')
@@ -46,13 +58,21 @@ class App{
 			return
 		}
 
-		let stream = document.getElementById('stream')
+		this.url = window.location.href
 
-		if(stream !== this.stream || (this.streamLastItem && !this.streamLastItem.parentElement)){
-			this.mountNewStream(stream)
+		if(this.isOnSubjectPage()){
+			let stream = document.getElementById('stream')
+
+			if(stream !== this.stream || (this.streamLastItem && !this.streamLastItem.parentElement)){
+				this.mountNewStream(stream)
+			}
+
+			this.updateCurrentlyViewed()
 		}
 
-		this.updateCurrentlyViewed()
+		if(this.isOnSettingsPage()){
+			Settings.attach()
+		}
 	}
 
 	static checkStreamExtension(){
@@ -131,6 +151,10 @@ class App{
 		thumb.className += ' seen'
 	}
 
+	static isOnSettingsPage(){
+		return window.location.href.includes('/settings/')
+	}
+
 	static isOnSubjectPage(){
 		let url = window.location.href
 		let path = url.split('/').slice(3).join('/')
@@ -139,7 +163,9 @@ class App{
 	}
 
 	static isLoading(){
-		return !document.getElementById('stream') || !!document.querySelector('#loader')
+		let mainView = document.getElementById('main-view')
+
+		return !mainView || mainView.children.length === 0 || !!document.querySelector('#loader')
 	}
 
 	static getItemIdFromThumb(thumb){
@@ -243,6 +269,22 @@ class Storage{
 		log('[Storage] created new chunk and updated index')
 	}
 
+	static getTotalCount(){
+		let n = 0
+
+		this.chunks.forEach(chunk => n += chunk.length)
+
+		return n
+	}
+
+	static getTotalMemory(){
+		let n = 0
+
+		this.chunks.forEach(chunk => n += 4 * CHUNK_SIZE)
+
+		return n
+	}
+
 	static flush(){
 		this.storeString('chunk' + this.currentChunk.id, this.currentChunk.encode())
 
@@ -268,6 +310,41 @@ class Storage{
 
 	static storeString(id, str){
 		window.localStorage.setItem(this.itemPrefix + id, str)
+	}
+
+	static export(){
+		let encoded = JSON.stringify(this.index) + '\r\n'
+
+		this.chunks.forEach(chunk => {
+			encoded += chunk.id + ':' + btoa(chunk.encode()) + '\r\n'
+		})
+
+		return encoded
+	}
+
+	static import(encoded){
+		try{
+			let lines = encoded.split('\r\n')
+			let index = JSON.parse(lines[0])
+
+			if(!index.chunkSize || !index.chunks)
+				return false
+
+			for(let i=0; i<index.chunks; i++){
+				let split = lines[i+1].split(':')
+				let b = split[1]
+				let chunk = Chunk.fromEncoded(atob(b))
+
+				for(let u=0; u<chunk.length; u++){
+					this.seen(chunk.view[u])
+				}
+			}
+
+			return true
+		}catch(e){
+			log(e)
+			return false
+		}
 	}
 }
 
@@ -320,6 +397,129 @@ class Chunk{
 
 	encode(){
 		return (String.fromCharCode.apply(null, this.codingView))
+	}
+}
+
+class Settings{
+	static attach(){
+		let exists = document.querySelector('#sch0ngesehen-settings')
+
+		if(!exists)
+			new Settings()
+	}
+
+	constructor(){
+		this.tabBar = document.querySelector('#main-view .tab-bar')
+		this.pane = document.querySelector('#main-view .pane')
+
+		this.link = document.createElement('a')
+		this.link.id = 'sch0ngesehen-settings'
+		this.link.href = '#'
+		this.link.textContent = 'sch0ngesehen'
+		this.link.addEventListener('click', e => {
+			e.preventDefault()
+			this.open()
+		})
+
+		this.tabBar.insertBefore(this.link, this.tabBar.querySelector('span'))
+
+		log('[Settings] attached')
+	}
+
+	open(){
+		Array.from(this.tabBar.querySelectorAll('a')).forEach(a => a.classList.remove('active'))
+		this.link.classList.add('active')
+
+		this.pane.innerHTML = ''
+		this.pane.append(this.createPanel())
+	}
+
+	offerExportedDownload(){
+		let trigger = document.createElement('a')
+		let text = Storage.export()
+		trigger.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+		trigger.setAttribute('download', 'sch0ngesehen-verlauf.txt');
+
+		trigger.style.display = 'none';
+		document.body.appendChild(trigger)
+		trigger.click()
+		document.body.removeChild(trigger)
+	}
+
+	import(upload){
+		let reader = new FileReader()
+
+		reader.onload = () => {
+			if(!Storage.import(reader.result)){
+				alert('Die Datei hat ein unerwartetes Format und kann nicht importiert werden. Hast du die richtige Datei ausgwählt?')
+				upload.value = ''
+			}else{
+				setTimeout(() => alert('Der Import war erfolgreich.'), 0)
+				this.open()
+			}
+		}
+
+		reader.readAsText(upload.files[0])
+	}
+
+	offerArmageddon(){
+		if(confirm('Möchtest du den gesamten Verlauf wirklich löschen? Dies ist irreversibel!')){
+			Storage.nuke()
+			this.open()
+		}
+	}
+
+
+	createPanel(){
+		let panel = document.createElement('div')
+		let info = document.createElement('p')
+		let exportButton = this.createButton('Verlauf exportieren (.txt)')
+		let importButton = this.createButton('Verlauf importieren (.txt)')
+		let nukeButton = this.createButton('Verlauf löschen')
+		let upload = document.createElement('input')
+
+		info.innerHTML 	= 'Insgesamt ' + Storage.getTotalCount().toLocaleString() + ' Hochlads angesehen.'
+						+ '<br>'
+						+ 'Lokaler Speicherbedarf: ' + this.formatBytes(Storage.getTotalMemory()) + '.'
+
+
+		exportButton.addEventListener('click', () => this.offerExportedDownload())
+		importButton.addEventListener('click', () => upload.click())
+		nukeButton.addEventListener('click', () => this.offerArmageddon())
+
+		upload.type = 'file'
+		upload.accept = 'text/plain'
+		upload.setAttribute('hidden', '1')
+		upload.addEventListener('change', () => this.import(upload))
+
+		panel.append(info)
+		panel.append(exportButton)
+		panel.append(importButton)
+		panel.append(nukeButton)
+		panel.append(upload)
+
+		return panel
+	}
+
+	createButton(label){
+		let button = document.createElement('input')
+
+		button.type = 'button'
+		button.classList.add('confirm')
+		button.value = label
+		button.style.display = 'block'
+		button.style.marginTop = '10px'
+
+		return button
+	}
+
+	formatBytes(b){
+		if(b < 1000)
+			return b.toLocaleString() + ' Bytes'
+		else if(b < 1000 * 1000)
+			return Math.round(b/1000) + ' KB'
+		else
+			return Math.round(b/(1000 * 1000)) + ' MB'
 	}
 }
 
